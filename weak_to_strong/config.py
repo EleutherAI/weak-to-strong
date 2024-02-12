4c9dfe19f107bb1e3645a25e89740aac0a706c0e
skar0 <oskar.hollinsworth@gmail.com>
2024-02-12 12:03:29 +0000

    Set model_parallel programmatically unless overriden

--- weak_to_strong/config.py
+++ weak_to_strong/config.py
@@ -5,100 +5,103 @@ from typing import Optional
 from weak_to_strong.loss import logconf_loss_fn, product_loss_fn, xent_loss, kl_loss
 
 
-@dataclass
 class ModelConfig:
-    name: str
-    default_lr: float
-    eval_batch_size: int
-    minibatch_size_per_device: Optional[int] = None
-    lora_modules: Optional[list[str]] = None
-    custom_kwargs: Optional[dict] = None
-    gradient_checkpointing: bool = False
-    model_parallel: bool = False
-    default_optimizer: str = "adam"
+
+    def __init__(
+        self, 
+        name: str, 
+        memory: float,
+        default_lr: float = 1e-5,
+        eval_batch_size: int = 32,
+        minibatch_size_per_device: Optional[int] = None,
+        lora_modules: Optional[list[str]] = None,
+        custom_kwargs: Optional[dict] = None,
+        gradient_checkpointing: bool = False,
+        model_parallel: Optional[bool] = None,
+        default_optimizer: str = "adam",
+    ):
+        self.per_device_ram = torch.cuda.get_device_properties(0).total_memory
+        self.n_devices = torch.cuda.device_count()
+        if model_parallel is None:
+            model_parallel = (
+                self.n_devices > 1 and self.per_device_ram < 2 * self.memory
+            )
+        self.name = name
+        self.memory = memory
+        self.default_lr = default_lr
+        self.eval_batch_size = eval_batch_size
+        self.minibatch_size_per_device = minibatch_size_per_device
+        self.lora_modules = lora_modules
+        self.custom_kwargs = custom_kwargs
+        self.gradient_checkpointing = gradient_checkpointing
+        self.model_parallel = model_parallel
+        self.default_optimizer = default_optimizer
 
 
 GPT_NEOX_LORA_MODULES = ["dense_h_to_4h", "dense_4h_to_h", "query_key_value"]
 GPT2_LORA_MODULES = ["c_fc", "c_proj", "c_attn"]
-per_device_ram = torch.cuda.get_device_properties(0).total_memory
+
 
 # NOTE learning rates are not particularly tuned, work somewhat reasonably at train batch size 32
 MODEL_CONFIGS = [
     ModelConfig(
         name="gpt2",
+        memory=550e6,
         default_lr=5e-5,
-        eval_batch_size=32,
         lora_modules=GPT2_LORA_MODULES,
     ),
     ModelConfig(
         name="gpt2-medium",
+        memory=1.5e9,
         default_lr=5e-5,
-        eval_batch_size=32,
         lora_modules=GPT2_LORA_MODULES,
     ),
     ModelConfig(
         name="gpt2-large",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=3.25e6,
         lora_modules=GPT2_LORA_MODULES,
     ),
     ModelConfig(
         name="gpt2-xl",
-        default_lr=1e-5,
+        memory=6.43e9,
         eval_batch_size=2,
         gradient_checkpointing=True,
         lora_modules=GPT2_LORA_MODULES,
-        # Should use model_parallel on V100s (note: ironically if you have a single V100
-        # it should run, but if you have multiple it won't run without model_parallel
-        # because of the overhead of data parallel training).
-        model_parallel=(per_device_ram < 35e9 and torch.cuda.device_count() > 1),
     ),
     ModelConfig(
         name="EleutherAI/pythia-70m",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=166e6,
         minibatch_size_per_device=32,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
     ),
     ModelConfig(
         name="EleutherAI/pythia-14m",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=53e6,
         minibatch_size_per_device=32,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
     ),
     ModelConfig(
         name="EleutherAI/pythia-160m-v0",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=375e6,
         minibatch_size_per_device=32,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
     ),
     ModelConfig(
         name="EleutherAI/pythia-410m",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=911e6,
         minibatch_size_per_device=32,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
     ),
     ModelConfig(
         name="EleutherAI/pythia-2.8b",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=5.68e9,
         minibatch_size_per_device=2,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
     ),
     ModelConfig(
         name="EleutherAI/pythia-12b",
-        default_lr=1e-5,
-        eval_batch_size=32,
+        memory=24e9,
         minibatch_size_per_device=2,  # this needs adjusting for GPU/dataset
-        model_parallel=False,
         lora_modules=GPT_NEOX_LORA_MODULES,
         custom_kwargs={
             "torch_dtype": torch.bfloat16
@@ -108,7 +111,7 @@ MODEL_CONFIGS = [
     ),
     ModelConfig(
         name="mistralai/Mistral-7B-v0.1",
-        default_lr=1e-5,
+        memory=15e9,
         eval_batch_size=2,
         lora_modules=[
             "up_proj",
@@ -120,7 +123,6 @@ MODEL_CONFIGS = [
         ],
         minibatch_size_per_device=1,  # this needs adjusting for GPU/dataset
         gradient_checkpointing=True,
-        model_parallel=False,
         custom_kwargs={
             "torch_dtype": torch.bfloat16  # we can only do this because we're using LoRA
             if torch.cuda.is_bf16_supported()
@@ -129,10 +131,9 @@ MODEL_CONFIGS = [
     ),
     ModelConfig(
         name="Qwen/Qwen-1_8B",
-        default_lr=1e-5,
+        memory=3.67e9,
         eval_batch_size=2,
         gradient_checkpointing=True,
-        model_parallel=(per_device_ram < 35e9 and torch.cuda.device_count() > 1),
         custom_kwargs={
             "trust_remote_code": True,
             "bf16": torch.cuda.is_bf16_supported(),
@@ -142,10 +143,9 @@ MODEL_CONFIGS = [
     ),
     ModelConfig(
         name="Qwen/Qwen-7B",
-        default_lr=1e-5,
+        memory=16e9,
         eval_batch_size=2,
         gradient_checkpointing=True,
-        model_parallel=True,
         # note: you will probably not be able to run this without many gpus
         custom_kwargs={
             "trust_remote_code": True,
@@ -156,10 +156,9 @@ MODEL_CONFIGS = [
     ),
     ModelConfig(
         name="Qwen/Qwen-14B",
-        default_lr=1e-5,
+        memory=30e9,
         eval_batch_size=2,
         gradient_checkpointing=True,
-        model_parallel=True,
         # note: you will probably not be able to run this bf16 support and without many gpus
         custom_kwargs={
             "trust_remote_code": True,
@@ -170,10 +169,9 @@ MODEL_CONFIGS = [
     ),
     ModelConfig(
         name="Qwen/Qwen-72B",
-        default_lr=1e-5,
+        memory=164e9,
         eval_batch_size=1,
         gradient_checkpointing=True,
-        model_parallel=True,
         # note: you will probably not be able to run this without bf16 support and many gpus
         custom_kwargs={
             "trust_remote_code": True,
